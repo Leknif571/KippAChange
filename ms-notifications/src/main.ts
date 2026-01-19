42acb39920defea07e702fe204d67188cf9c8035
Noe <noeziadi@outlook.com>
2026-01-19 15:34:31 +0100

    Ajout ms-notifications

---
+++ ms-notifications/src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://rabbitmq:5672'],
+      queue: 'notifications_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  
+  await app.listen(3001, '0.0.0.0'); 
+  
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
