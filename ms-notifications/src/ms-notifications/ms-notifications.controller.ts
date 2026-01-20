1d3f154a5e1fbaf4c9547ab155eb7bfb090a2cd9
Noe <noeziadi@outlook.com>
2026-01-20 13:21:20 +0100

    Ajout de l'event 'bet_created'

--- ms-notifications/src/ms-notifications/ms-notifications.controller.ts
+++ ms-notifications/src/ms-notifications/ms-notifications.controller.ts
@@ -9,11 +9,19 @@ export class NotificationsController {
   // Dès qu'un message avec le motif "user_created" arrive, cette fonction se lance
   @EventPattern('user_created')
   handleUserCreated(@Payload() data: any, @Ctx() context: RmqContext) {
-    console.log(`⚡️ Event Reçu via RabbitMQ :`, data);
-    
-    // Exemple : Créer la notif automatiquement
+    console.log(`⚡️ Event [UserCreated] Reçu via RabbitMQ :`, data);
+
     if (data.userId && data.email) {
-        this.notificationsService.create(data.userId, `Bienvenue ${data.email} !`);
+      this.notificationsService.create(data.userId, `Bienvenue ${data.email} !`);
+    }
+  }
+
+  @EventPattern('bet_created')
+  handleBetCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    console.log(`⚡️ Event [BetCreated] Reçu via RabbitMQ :`, data);
+
+    if (data.userId && data.matchId) {
+      this.notificationsService.create(data.userId, `Bet ${data.matchId} du joueur ${data.userId} avec la probabilité ${data.odds} à payer ${data.amount} !`);
     }
   }
 }
